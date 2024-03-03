35d8a978a267c223443024d77415830f51ad7851
Janne Palo <janne.palo@gmail.com>
2024-03-03 03:14:03 +0200

    File extension update

--- .bashmenu.sh
+++ .bashmenu.sh
@@ -1,51 +1,51 @@
 #!/usr/bin/env bash
 
-## Collect the files in the bash menu folder to an array $files
-MYPATH="`dirname \"$0\"`"
-cd $MYPATH
-curdir=`pwd`
-files=( * )
+## Collect the files with .menuitem extension in the bash menu folder to an array $files
+MYPATH="$(dirname "$0")"
+cd "$MYPATH"
+curdir=$(pwd)
+files=(*.menuitem)
 ## Enable extended globbing. This lets us use @(foo|bar) to
 ## match either 'foo' or 'bar'.
 shopt -s extglob
 
 ## Start building the string to match against.
-string="@(${files[0]}"
+string="@(${files[0]%.menuitem}"  # Remove the .menuitem extension for matching
 ## Add the rest of the files to the string
-for((i=1;i<${#files[@]};i++))
+for((i=1; i<${#files[@]}; i++))
 do
-    string+="|${files[$i]}"
+    string+="|${files[$i]%.menuitem}"  # Remove the .menuitem extension
 done
-## Close the parenthesis. $string is now @(file1|file2|...|fileN)
 string+=")"
 
-## Show the menu. This will list all files
-#echo "q) quit"
-#echo "n) new"
-select file in "${files[@]}" "quit" "new"
+## Modify the display of files to exclude the .menuitem extension
+display_files=("${files[@]/%.menuitem/}")
+
+## Show the menu. This will list all files without the .menuitem extension
+select file in "${display_files[@]}" "quit" "new"
 do
     case $file in
-    ## If the choice is one of the files (if it matches $string)
     $string)
-        ## Do something here
-        cat "$file"
-	command=`cat $file`
-	exec $command
+        # Convert back to the actual filename to read the command
+        actual_file="$file.menuitem"
+        command=$(cat "$actual_file")
+        $command
+        exit
         ;;
     "quit")
-        ## Exit
-        exit;;
+        exit
+        ;;
     "new")
-	echo "Type in the command f.ex: ssh -p 2222 pi@123.123.123.1"
-	read -p 'command: ' new_command
-	echo Command was: $new_command
-	read -p 'Nickname for this command: ' nick
-	touch ./$nick
-	echo $new_command > ./$nick
-	exec ./.bashmenu.sh
-	;;
+        echo "Type in the command f.ex: ssh -p 2222 pi@123.123.123.1"
+        read -p 'command: ' new_command
+        echo "Command was: $new_command"
+        read -p 'Nickname for this command: ' nick
+        touch "./$nick.menuitem"  # Save with .menuitem extension
+        echo "$new_command" > "./$nick.menuitem"
+        exec ./.bashmenu.sh  # Return to the menu; adjust this as needed
+        ;;
     *)
-        file=""
-        echo "Please choose a number from 1 to $((${#files[@]}+2))";;
+        echo "Please choose a number from 1 to $((${#display_files[@]} + 2))"
+        ;;
     esac
 done
